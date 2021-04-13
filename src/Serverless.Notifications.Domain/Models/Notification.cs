27a35d7b07541f559b8743585f22a1d61a7a4e60
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-14 00:02:07 +0530

    feat: add required new props

--- src/Serverless.Notifications.Domain/Models/Notification.cs
+++ src/Serverless.Notifications.Domain/Models/Notification.cs
@@ -8,6 +8,8 @@ namespace Serverless.Notifications.Domain.Models
         public Guid Id { get; set; } = Guid.NewGuid();
         public NotificationType NotificationType { get; set; }
         public DateTime TriggeredDateTime { get; set; } = DateTime.UtcNow;
+        public bool IsScheduled { get; set; }
+        public DateTime ScheduledDateTime { get; set; }
         public string Body { get; set; } = string.Empty;
     }
 }
