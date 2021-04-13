d0b032f35134d61e95f6f943ca9e907a9b355129
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-14 00:02:54 +0530

    feat: add notifications router function

---
+++ src/Serverless.Notifications.Api/NotificationRouter.cs
@@ -0,0 +1,25 @@
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using Serverless.Notifications.Application.Common.Interfaces;
+using Serverless.Notifications.Domain.Models;
+using System.Threading.Tasks;
+
+namespace Serverless.Notifications.Api
+{
+    public class NotificationRouter
+    {
+        private readonly INotificationPoolRouter _notificationPoolRouter;
+
+        public NotificationRouter(INotificationPoolRouter notificationPoolRouter)
+        {
+            _notificationPoolRouter = notificationPoolRouter;
+        }
+
+        [FunctionName("NotificationRouter")]
+        public async Task Run([QueueTrigger("notification-pool")]string queueMessage)
+        {
+            Notification notification = JsonConvert.DeserializeObject<Notification>(queueMessage);
+            await _notificationPoolRouter.RouteNotification(notification);
+        }
+    }
+}
