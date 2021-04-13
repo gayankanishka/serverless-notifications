3c105bffb92309066d387851f8a4eaa896d327c6
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-13 23:07:06 +0530

    feat: add initial application layer

---
+++ src/Serverless.Notifications.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Serverless.Notifications.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
