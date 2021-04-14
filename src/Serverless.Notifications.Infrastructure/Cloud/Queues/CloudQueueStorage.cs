a8e0313d5c552e090d2947ece75896d5ebda6a9c
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-14 11:56:11 +0530

    feat: add new required methods for queue handling

--- src/Serverless.Notifications.Infrastructure/Cloud/Queues/CloudQueueStorage.cs
+++ src/Serverless.Notifications.Infrastructure/Cloud/Queues/CloudQueueStorage.cs
@@ -1,4 +1,6 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
 using Serverless.Notifications.Application.Common.Interfaces;
 using System.Threading.Tasks;
 
@@ -7,6 +9,7 @@ namespace Serverless.Notifications.Infrastructure.Cloud.Queues
     public class CloudQueueStorage : INotificationPoolQueue, IEmailQueue, ISmsQueue, INotificationScheduleQueue
     {
         private readonly QueueClient _queueClient;
+        private readonly QueueClient _poisonQueueClient;
 
         public CloudQueueStorage(string connectionString, string queueName)
         {
@@ -14,10 +17,15 @@ namespace Serverless.Notifications.Infrastructure.Cloud.Queues
             {
                 MessageEncoding = QueueMessageEncoding.Base64
             });
+
+            _poisonQueueClient = new QueueClient(connectionString, $"{queueName}-poison", new QueueClientOptions
+            {
+                MessageEncoding = QueueMessageEncoding.Base64
+            });
         }
 
-        /// <inheritdoc cref="InsertMessageAsync"/>
-        public async Task InsertMessageAsync(string message)
+        /// <inheritdoc cref="SendMessageAsync"/>
+        public async Task SendMessageAsync(string message)
         {
             if (string.IsNullOrWhiteSpace(message))
             {
@@ -30,6 +38,61 @@ namespace Serverless.Notifications.Infrastructure.Cloud.Queues
             {
                 await _queueClient.SendMessageAsync(message);
             }
+        }
+
+        public async Task SendMessageToPoisonQueueAsync(QueueMessage message)
+        {
+            await _poisonQueueClient.CreateIfNotExistsAsync();
+
+            if (await _poisonQueueClient.ExistsAsync())
+            {
+                await _poisonQueueClient.SendMessageAsync(message.Body);
+                await DeleteMessagesAsync(message);
+            }
+        }
+
+        /// <inheritdoc cref="ReceiveMessagesAsync"/>
+        public async Task<QueueMessage[]> ReceiveMessagesAsync(int messageCount = 32)
+        {
+            if (await _queueClient.ExistsAsync())
+            {
+                return await _queueClient.ReceiveMessagesAsync(messageCount);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc cref="PeekMessagesAsync"/>
+        public async Task<PeekedMessage[]> PeekMessagesAsync(int messageCount = 32)
+        {
+            if (await _queueClient.ExistsAsync())
+            {
+                return await _queueClient.PeekMessagesAsync(messageCount);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc cref="DeleteMessagesAsync"/>
+        public async Task DeleteMessagesAsync(QueueMessage message)
+        {
+            if (await _queueClient.ExistsAsync())
+            {
+                await _queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+
+            }
+        }
+
+        public async Task<int> GetApproximateMessagesCount()
+        {
+            if (!await _queueClient.ExistsAsync())
+            {
+                return 0;
+            }
+
+            QueueProperties properties = await _queueClient.GetPropertiesAsync();
+
+            return properties.ApproximateMessagesCount;
         }
     }
 }
