02f145d13ccdc4c47c865f7672c95055cf3e8297
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-15 10:36:14 +0530

    fix: add temp fix until frombody attribute issue is resolved

--- src/Serverless.Notifications.AzureFunctions/Functions/NotificationController.cs
+++ src/Serverless.Notifications.AzureFunctions/Functions/NotificationController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Newtonsoft.Json;
 using Serverless.Notifications.Application.Common.Interfaces;
 using Serverless.Notifications.Domain.Models;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Serverless.Notifications.AzureFunctions.Functions
@@ -20,11 +22,13 @@ namespace Serverless.Notifications.AzureFunctions.Functions
         [FunctionName("PostNotifications")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "notifications")]
-            [FromBody]Notification notification)
+            HttpRequest request)
         {
-            string message = JsonConvert.SerializeObject(notification);
+            // TODO: Use [FromBody] attribute once issue is fixed
+            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+            Notification notification = JsonConvert.DeserializeObject<Notification>(requestBody);
 
-            await _notificationPool.SendMessageAsync(message);
+            await _notificationPool.SendMessageAsync(requestBody);
 
             return new AcceptedResult("notifications", notification.Id);
         }
