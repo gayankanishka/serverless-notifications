485e0d5f62a37be755c6c4f4b1b0693be8f38e66
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-24 22:53:55 +0530

    table config

---
+++ src/Serverless.Notifications.Infrastructure/Cloud/Tables/TableConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Serverless.Notifications.Application.Common.Interfaces;
+using Serverless.Notifications.Domain.TableEntities;
+
+namespace Serverless.Notifications.Infrastructure.Cloud.Tables
+{
+    public class TableConfiguration : ITableConfiguration
+    {
+        private readonly ICloudStorageTable _cloudStorageTable;
+        
+        public TableConfiguration(ICloudStorageTable cloudStorageTable)
+        {
+            _cloudStorageTable = cloudStorageTable;
+            _cloudStorageTable.TableName = "Configurations";
+            _cloudStorageTable.PartitionKey = "Configuration";
+        }
+        
+        public async Task<string> GetSettingAsync(string key, string partitionKey = null)
+        {
+            ConfigurationEntity config = await _cloudStorageTable.GetTableEntityAsync<ConfigurationEntity>(key, partitionKey);
+            return config.ConfigurationValue;
+        }
+    }
+}
