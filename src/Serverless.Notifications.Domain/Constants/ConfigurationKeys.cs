863afbf1a9d5aaf421f8e222d82b7f7c264af164
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2021-04-24 14:07:41 +0530

    feat: add configuration consts

---
+++ src/Serverless.Notifications.Domain/Constants/ConfigurationKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Serverless.Notifications.Domain.Constants
+{
+    public static class ConfigurationKeys
+    {
+        public const string NotificationPoolQueueName = "NotificationPool";
+        public const string EmailQueueName = "EmailQueue";
+        public const string SmsQueueName = "SmsQueue";
+        public const string ScheduleQueueName = "ScheduleQueue";
+    }
+}
